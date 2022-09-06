7860cc7a0409109f7cc404ff457889746c619ccf
Renata Svikle <renata.svikle@inbox.lv>
2022-09-06 21:44:46 +0300

    DraftTicketPriceController - without response entity

--- src/main/java/com/travel/ticket/controller/DraftTicketPriceController.java
+++ src/main/java/com/travel/ticket/controller/DraftTicketPriceController.java
@@ -2,11 +2,9 @@ package com.travel.ticket.controller;
 
 import com.travel.ticket.dto.DraftTicketPriceRequestDto;
 import com.travel.ticket.dto.DraftTicketPriceResponseDto;
-import com.travel.ticket.service.DraftTicketRequestValidationService;
 import com.travel.ticket.service.DraftTicketPriceCalculationService;
+import com.travel.ticket.service.DraftTicketRequestValidationService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +17,9 @@ public class DraftTicketPriceController {
     private final DraftTicketRequestValidationService draftTicketRequestValidationService;
 
     @PostMapping({"/draftTicketPrice"})
-    public ResponseEntity<DraftTicketPriceResponseDto> getDraftTicketPrice(
+    public DraftTicketPriceResponseDto getDraftTicketPrice(
             @RequestBody DraftTicketPriceRequestDto draftTicketPriceRequestDto) {
         draftTicketRequestValidationService.validateDraftTicketPriceRequest(draftTicketPriceRequestDto);
-        return new ResponseEntity<>(draftTicketPriceCalculationService.getAllTicketDraftPrice(draftTicketPriceRequestDto), HttpStatus.OK);
+        return draftTicketPriceCalculationService.getAllTicketDraftPrice(draftTicketPriceRequestDto);
     }
 }
